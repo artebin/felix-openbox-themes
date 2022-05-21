0200e00ec9b253783a42e69f0248b977f99da866
Nicolas James <njames@voltaire>
2022-05-21 16:42:56 +0200

    fixed typo in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 THEMES_DIRECTORY="${HOME}/.local/share/themes"
-if [[ -d "${THEMES_DIRECTORY}" ]]; then
+if [[ ! -d "${THEMES_DIRECTORY}" ]]; then
 	mkdir -p "${THEMES_DIRECTORY}"
 fi
 cp -r themes/* "${THEMES_DIRECTORY}"
