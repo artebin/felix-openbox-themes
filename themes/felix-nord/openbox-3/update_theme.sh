27e700065d79aed890ef794f9e08a2d4cae5bd0c
Nicolas James <njames@voltaire>
2022-06-07 12:15:35 +0200

    generate theme overview in html

--- themes/felix-nord/openbox-3/update_theme.sh
+++ themes/felix-nord/openbox-3/update_theme.sh
@@ -65,11 +65,11 @@ THEMERC_PROPERTY_NAME_LIST_MENU_DISABLED_FONT_COLOR=( "menu.items.disabled.text.
 
 THEMERC_PROPERTY_VALUE_DELIMITER=":"
 
-escape_sed_pattern(){
+function escape_sed_pattern(){
 	printf "${1}" | sed -e 's/[\\&]/\\&/g' | sed -e 's/[\/&]/\\&/g'
 }
 
-update_line_based_on_prefix(){
+function update_line_based_on_prefix(){
 	PREFIX_TO_SEARCH="${1}"
 	LINE_REPLACEMENT_VALUE="${2}"
 	FILE_PATH="${3}"
@@ -83,6 +83,33 @@ update_line_based_on_prefix(){
 	fi
 }
 
+function key_value_retriever(){
+	LINE="${1}"
+	if [[ -z "${LINE}" ]]; then
+		printf "ERROR: LINE should not be empty\n"
+		return
+	fi
+	KEY_VARNAME="${2}"
+	if [[ -z "${KEY_VARNAME}" ]]; then
+		printf "ERROR: KEY_VARNAME should not be empty\n"
+		return
+	fi
+	VALUE_VARNAME="${3}"
+	if [[ -z "${VALUE_VARNAME}" ]]; then
+		printf "ERROR: VALUE_VARNAME should not be empty\n"
+		return
+	fi
+	INDEX_OF_FIRST_EQUAL=$(expr index "${LINE}" :)
+	KEY=""
+	VALUE=""
+	if [[ ${INDEX_OF_FIRST_EQUAL} -ne -1 ]]; then
+		KEY="${LINE:0:${INDEX_OF_FIRST_EQUAL} - 1}"
+		VALUE="${LINE:${INDEX_OF_FIRST_EQUAL}}"
+	fi
+	export "${KEY_VARNAME}"="${KEY}"
+	export "${VALUE_VARNAME}"="${VALUE}"
+}
+
 function apply_template(){
 	for THEME_VARIABLE_NAME in "${THEME_VARIABLE_NAME_LIST[@]}"; do
 		THEMERC_PROPERTY_NAME_LIST_NAME="THEMERC_PROPERTY_NAME_LIST_${THEME_VARIABLE_NAME}"
@@ -94,8 +121,36 @@ function apply_template(){
 	done
 }
 
+function generate_themerc_html_overview(){
+	FILE_PATH="${1}"
+	HTML_OVERVIEW="<html><style>table,th,td{border:1px solid black;border-collapse:collapse;} td{padding:5px;}</style>"
+	HTML_OVERVIEW+="<table><tr><th>Key</th><th>Value</th></tr>"
+	while IFS= read -r LINE; do
+		if [[ ! -z "${LINE}" ]]; then
+			key_value_retriever "${LINE}" "LINE_KEY" "LINE_VALUE"
+			if [[ ! -z "${LINE_KEY}" ]]; then
+				HTML_OVERVIEW+="<tr>"
+				HTML_OVERVIEW+="<td>${LINE_KEY}</td>"
+				
+				# If key ends with '.color' then render the color
+				if [[ "${LINE_KEY}" == *.color* ]]; then
+					HTML_OVERVIEW+="<td><span style=\"display:inline-block;width:120px;height:40px;background-color:${LINE_VALUE};\">&nbsp;</span> ${LINE_VALUE}</td>"
+				else
+					HTML_OVERVIEW+="<td>${LINE_VALUE}</td>"
+				fi
+				
+				HTML_OVERVIEW+="</tr>"
+			fi
+		fi
+	done < "${FILE_PATH}"
+	HTML_OVERVIEW+="</table></html>"
+	echo "${HTML_OVERVIEW}" >"${FILE_PATH}.overview.html"
+}
+
 cat themerc | sort | uniq > themerc.sorted
 rm -f themerc
 mv themerc.sorted themerc
 apply_template
+generate_themerc_html_overview themerc
 openbox --reconfigure
+
